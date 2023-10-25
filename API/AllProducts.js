4d7f6c565be63e76315c1e46d35265c5c3a015f0
Peter-Kang <pkang93@gmail.com>
2023-10-25 14:16:11 -0400

    Filter the changes

--- API/AllProducts.js
+++ API/AllProducts.js
@@ -100,7 +100,7 @@ async function getRandomProduct(button)
     const randomIndex = Math.floor(Math.random()*ALL_PRODUCTS.length);
     console.log(randomIndex);
     let resultObject = ALL_PRODUCTS[randomIndex];
-    const resultDisplay = JSON.stringify(resultObject.title)
+    const resultDisplay = JSON.stringify(resultObject.title).replaceAll("\"","")
     + (resultObject.body_html)
     + "<div'><img width='25%' src='"+JSON.stringify(resultObject.images[0].src).replace("\"",'')+"/resize=width:200'></img></div>";
     document.getElementById("tempFill").innerHTML = resultDisplay;
