6af9ff72ea13039a8071da49b5ab25d2edc5d2b4
Peter Kang <pxk7062@rit.edu>
2023-10-26 15:55:57 -0400

    Updated info when button is pressed

--- CatButton/CatButton.js
+++ CatButton/CatButton.js
@@ -2,10 +2,11 @@
 async function getRandomProduct(button) {
   // Randomly select a tea
   const randomIndex = Math.floor(Math.random() * ALL_PRODUCTS.length);
-  console.log(randomIndex);
   let resultObject = ALL_PRODUCTS[randomIndex];
+  let teaName = JSON.stringify(resultObject.title).replaceAll('"', "");
+  console.info(randomIndex, teaName);
   const resultDisplay =
-    JSON.stringify(resultObject.title).replaceAll('"', "") +
+    teaName +
     resultObject.body_html +
     "<div'><img width='25%' src='" +
     JSON.stringify(resultObject.images[0].src).replace('"', "") +
