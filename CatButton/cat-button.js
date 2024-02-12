75a0b253a2105d2e75fa149c81c65aa35b4af683
Peter Kang <pxk7062@rit.edu>
2024-02-12 12:36:09 -0500

    inital bacal animation

--- CatButton/cat-button.js
+++ CatButton/cat-button.js
@@ -1,6 +1,6 @@
 function catImageInteraction(catbutton) {
   const category = catbutton.querySelector("cat-img").getAttribute("category");
-  let cat_text = "";
+  let cat_text = ""; // default it to clear
   if (category === "5") {
     //xexe
     //check if xexe is home 1 in 24
@@ -10,8 +10,12 @@ function catImageInteraction(catbutton) {
     }
   } else if (category === "7") {
     //bakal
+    const appears = Math.floor(Math.random() * 5) == 0; // 20% chance he appears
+    if (appears) {
+      cat_text = "bacal";
+    }
   } else {
-    cat_text = "";
+    cat_text = ""; //clear if there was anything there before
   }
   catbutton.querySelector("cat-img").setAttribute("cat", cat_text);
 }
