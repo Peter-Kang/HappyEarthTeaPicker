4ed14a5ce7b8cda11c9b23659269a33a2298316c
Peter-Kang <pkang93@gmail.com>
2024-02-17 14:54:42 -0500

    Inital Documents

--- README.md
+++
@@ -1,9 +0,0 @@
-# TeaPicker
-App for [Happy Earth Tea](https://happyearthtea.com/)
-
-Randomly picks a tea for you
-
-To run it locally cd to the directory and run:
-```
-npx serve .
-```
--- Util/Randomizer.js
+++
@@ -1,8 +0,0 @@
-function RandomlySelectTeaFromArray(array) {
-  const randomIndex = Math.floor(Math.random() * array.length);
-  const resultObject = array[randomIndex];
-  resultObject.title = JSON.stringify(resultObject.title).replaceAll('"', "");
-  //log for info purposes
-  console.info(randomIndex, resultObject.title);
-  return resultObject;
-}
